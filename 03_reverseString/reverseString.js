ca283f52f64e7ed69ab76aad5018955ad2486502
teddy <teddytilahun02@gmail.com>
2025-03-07 14:35:35 +0300

    solved 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  array = [];
+  array.s;
+  array = word.split("");
+  for (i = 0; i < array.length / 2; i++) {
+    // the last elements is equal to length of the array minus 01 mines and minus i
+    lastElement = array.length - 1 - i;
+    temp = array[i];
+    array[i] = array[lastElement];
+    array[lastElement] = temp;
+    console.log(array[i], array[lastElement]);
+  }
+  return array.join("");
 };
 
 // Do not edit below this line
