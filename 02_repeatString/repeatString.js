bd326ccf58a21d7616eb4de453c599183ba5e876
teddy <teddytilahun02@gmail.com>
2025-03-07 14:04:16 +0300

    solved 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (word, n) {
+  if (n < 0) {
+    return "ERROR";
+  }
+  if (word == "") {
+    return "";
+  }
+  result = "";
+  for (i = 0; i < n; i++) {
+    result += word;
+  }
+  return result;
 };
 
 // Do not edit below this line
