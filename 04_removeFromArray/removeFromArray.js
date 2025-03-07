acca4502c18c439888dac5739a65b064426f2caf
teddy <teddytilahun02@gmail.com>
2025-03-07 15:28:21 +0300

    solved 04_removefromarray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  hash = new Map();
+  for (const n of args) {
+    hash.set(n, 0);
+  }
+  array = array.filter((number) => {
+    return hash.get(number) != 0;
+  });
+  return array;
 };
 
 // Do not edit below this line
